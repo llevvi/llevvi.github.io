781a1e44080d6992a142ce30f66cefc68aee7044
Leandro <=>
2018-09-12 00:26:50 -0300

    Creates a file for constants

--- src/bio.js
+++ src/bio.js
@@ -1,28 +1,6 @@
 import React from 'react';
 import SocialMediaList from './socialMediaList';
-
-const SOCIAL_ICONS_AND_LINKS = [
-  {
-    'glyphicon': 'fa fa-linkedin fa-2x',
-    'url': 'https://www.linkedin.com/in/leandrovicentesilva/',
-    'name': 'linkedin'
-  },
-  {
-    'glyphicon': 'fa fa-github fa-2x',
-    'url': 'https://github.com/llevvi/',
-    'name': 'github'
-  },
-  {
-    'glyphicon': 'fa fa-youtube-play fa-2x',
-    'url': 'http://youtube.com/levvitalk',
-    'name': 'youtube'
-  },
-  {
-    'glyphicon': 'fa fa-instagram fa-2x',
-    'url': 'http://instagram.com/levvitalk',
-    'name': 'instagram'
-  }
-];
+import { SOCIAL_ICONS_AND_LINKS } from './constants';
 
 export default function Bio(props) {
     return (

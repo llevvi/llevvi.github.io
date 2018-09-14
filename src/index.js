99d239e673df5cb554b61f0297d6b54d29882eb5
Leandro <=>
2018-09-14 00:26:47 -0300

    Setup css loading

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Bio from './bio.js';
+import './stylesheets/style.css';
 
 ReactDOM.render(
 <Bio name="Leandro Vicente" tagline="Software Developer and Creator" />

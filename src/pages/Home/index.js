7b8f52e7aeb1f3f53cd4e3293059009e7c882d0d
Leandro <=>
2018-11-23 11:42:20 -0200

    Clears out directory to start a new project using create-react-app

--- src/pages/Home/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Bio from '../../components/Bio'
-import './style.css'
-
-const Home = () => 
-  <Bio name="Leandro Vicente" tagline="Software Developer and Creator" />
-    
-export default Home 
--- src/pages/YouTubeList/index.js
+++
@@ -1,3 +0,0 @@
-const YouTubeList = () => window.location.replace("https://goo.gl/forms/A3zRVgzeH3BMD2FJ3")
-
-export default YouTubeList
